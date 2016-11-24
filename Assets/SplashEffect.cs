00c07862d7fd1ccd8e0e4dbd7f26fd50860af0f7
Jesper Turesson <jturesson90@gmail.com>
2016-11-24 17:06:38 +0100

    Removed unimportant assets and fixed camera angle
    Camera angle is now 33 on x

---
+++ Assets/SplashEffect.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashEffect : MonoBehaviour
+{
+  public ParticleSystem pl;
+  void Start()
+  {
+    print("start");
+  }
+  void OnTriggerEnter(Collider coll)
+  {
+    print("mmm");
+    if (coll.transform.tag.Equals("Player"))
+    {
+      pl.transform.position = coll.transform.position;
+      pl.gameObject.SetActive(false);
+      pl.gameObject.SetActive(true);
+
+    }
+  }
+}
